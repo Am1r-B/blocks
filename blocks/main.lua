7375775e284e6d2abfeb72f6f37922214bd1804a
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-08-04 23:07:44 +0430

    setting block color

--- blocks/main.lua
+++ blocks/main.lua
@@ -11,12 +11,34 @@ function love.load()
       inert[y][x] = ' '
     end
   end
+  
+  -- Temporary
+  inert[18][1] = 'i'
+  inert[17][2] = 'j'
+  inert[16][3] = 'l'
+  inert[15][4] = 'o'
+  inert[14][5] = 's'
+  inert[13][6] = 't'
+  inert[12][7] = 'z'
 end
 
 function love.draw()
   for y = 1, gridYCount do
     for x = 1, gridXCount do
-      love.graphics.setColor(.87, .87, .87)
+      local colors = {
+        [' '] = {.87, .87, .87},
+        i = {.47, .76, .94},
+        j = {.93, .91, .42},
+        l = {.49, .85, .76},
+        o = {.92, .69, .47},
+        s = {.83, .54, .93},
+        t = {.97, .58, .77},
+        z = {.66, .83, .46}
+      }
+      local block = inert[y][x]
+      local color = colors[block]
+      love.graphics.setColor(color)
+      
       local pieceSize = 20
       local pieceDrawSize = pieceSize - 1
       love.graphics.rectangle(
