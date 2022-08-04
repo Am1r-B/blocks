a4054ba97a43c97a946a31a41378f6b9e133ec20
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-08-04 23:04:28 +0430

    storing inert blocks

--- blocks/main.lua
+++ blocks/main.lua
@@ -1,10 +1,21 @@
 function love.load()
   love.graphics.setBackgroundColor(1, 1, 1)
+  
+  gridXCount = 10
+  gridYCount = 18
+  
+  inert = {}
+  for y = 1, gridYCount do
+    inert[y] = {}
+    for x = 1, gridXCount do
+      inert[y][x] = ' '
+    end
+  end
 end
 
 function love.draw()
-  for y = 1, 18 do
-    for x = 1, 10 do
+  for y = 1, gridYCount do
+    for x = 1, gridXCount do
       love.graphics.setColor(.87, .87, .87)
       local pieceSize = 20
       local pieceDrawSize = pieceSize - 1
